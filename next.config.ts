d237f5354dfdd0de10386eb7287d0b57f747ba5e
Kashaf Aman <166901772+kashaf0007@users.noreply.github.com>
2025-02-03 14:30:13 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,12 @@ const nextConfig: NextConfig = {
   domains:['cdn.sanity.io']
  },
  
-    eslint: {
-      ignoreDuringBuilds: true,
-  }
-  
+    typescript:{
+ignoreBuildErrors:true,
+},
 };
+     
+  
+
 
 export default nextConfig;
